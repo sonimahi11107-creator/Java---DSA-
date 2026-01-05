e6cffa12eb3b6b191028bcf7d5bbe4ac577b58db
Mahi Soni <sonimahi11107@gmail.com>
2026-01-05 20:54:10 +0530

    Add files via upload

---
+++ BubbleSorting.java
@@ -0,0 +1,30 @@
+public class BubbleSorting {
+
+    public static void bubbleSorting(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+
+
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {10,91, 50, 52, 79 };
+        bubbleSorting(arr);
+        System.out.println("Result of bubble sorting");
+
+        for (int num : arr){
+            System.out.println(num+" ");
+        }
+    }
+}
